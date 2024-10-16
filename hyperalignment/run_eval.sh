a4aa0d40064b48999ed07c0bf580ae253781ead0
jaisidhsingh <jaisidhsingh@gmail.com>
2024-10-16 15:41:17 +0200

    set up shell script for evals todo

--- hyperalignment/run_eval.sh
+++ hyperalignment/run_eval.sh
@@ -1,1 +1,16 @@
-# TODO
+python3 evaluation.py \
+    --exp-name="vit_small_patch16_224" \
+    --seed=0 \
+    --run-type="sep" \
+    --epoch=1 \
+    --encoder-index=0 \
+    --benchmarks="imagenet1k";
+
+
+python3 evaluation.py \
+    --exp-name="norm_c_32" \
+    --seed=0 \
+    --run-type="mm" \
+    --epoch=1 \
+    --encoder-index=0 \
+    --benchmarks="imagenet1k";
