f6ea26072efc5f770dc6d2d0644f87586b1fa08d
jaisidhsingh <jaisidhsingh@gmail.com>
2024-12-22 17:54:28 +0100

    after ood test 1, get ref separate eval

--- hyperalignment/run_eval.sh
+++ hyperalignment/run_eval.sh
@@ -1,7 +1,15 @@
+# python3 evaluation.py \
+#     --exp-name="vit_small_patch16_224" \
+#     --seed=0 \
+#     --run-type="sep" \
+#     --epoch=10 \
+#     --encoder-index=0 \
+#     --benchmarks="cifar10,cifar100,imagenet1k,mscoco";
+
 python3 evaluation.py \
-    --exp-name="vit_small_patch16_224" \
+    --exp-name="flexivit_small.300ep_in1k" \
     --seed=0 \
     --run-type="sep" \
     --epoch=10 \
-    --encoder-index=0 \
+    --encoder-index=5 \
     --benchmarks="cifar10,cifar100,imagenet1k,mscoco";
