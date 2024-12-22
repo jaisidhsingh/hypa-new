1f43b19e9ff4c20967710740e01c56765bbc8b83
jaisidhsingh <jaisidhsingh@gmail.com>
2024-12-22 23:43:25 +0100

    eval init ood

--- hyperalignment/run_eval.sh
+++ hyperalignment/run_eval.sh
@@ -18,7 +18,7 @@ python3 evaluation.py \
     --exp-name="flexivit_small.300ep_in1k" \
     --seed=0 \
     --run-type="ood" \
-    --ood-results-path="ood_attempt_10k.pt" \
+    --ood-results-path="ood_attempt_10k_avg.pt" \
     --epoch=10 \
     --encoder-index=5 \
     --benchmarks="cifar10,cifar100,imagenet1k,mscoco";
