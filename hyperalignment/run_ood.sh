c2b63e77f52cbd00cebc1cbd19713fa83d2a98ea
Diganta Misra <mishradiganta91@gmail.com>
2024-12-29 16:41:29 -0500

    transfer

--- hyperalignment/run_ood.sh
+++ hyperalignment/run_ood.sh
@@ -6,7 +6,8 @@ python3 evaluation.py \
     --exp-name="beit_base_patch16_224.in22k_ft_in22k_in1k" \
     --seed=0 \
     --run-type="ood" \
-    --ood-results-path="ood_attempt_10k_avg.pt" \
+    --ood-results-path="ood_attempt_10k_avg_beit_768.pt" \
     --epoch=10 \
     --encoder-index=15 \
     --benchmarks="cifar10,cifar100,imagenet1k,mscoco";
+# write test
