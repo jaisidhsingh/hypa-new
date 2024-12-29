5b41598aff00e4bc6759c21f876d25efb243543c
jaisidhsingh <jaisidhsingh@gmail.com>
2024-12-29 23:22:01 +0100

    check another 768

--- hyperalignment/run_ood.sh
+++ hyperalignment/run_ood.sh
@@ -1,12 +1,12 @@
 python3 ood_autodecoding.py \
-    --image-encoder="deit3_base_patch16_224.fb_in22k_ft_in1k" \
+    --image-encoder="maxvit_base_tf_224.in1k" \
     --image-embed-dim=768 \
     --learning-rate=1e-4 \
     --save-path="ood_attempt_10k_avg_visf_384.pt" \
     --batch-size=8;
 
 python3 evaluation.py \
-    --exp-name="deit3_base_patch16.fb_in22k_ft_in1k" \
+    --exp-name="maxvit_base_tf_224.in1k" \
     --seed=0 \
     --run-type="ood" \
     --ood-results-path="ood_attempt_10k_avg_visf_384.pt" \
