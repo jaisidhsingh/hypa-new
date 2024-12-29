5bcbb66a9b99b2d41d465b4e331f1029768ff907
jaisidhsingh <jaisidhsingh@gmail.com>
2024-12-29 15:57:24 +0100

    debug+adjust for groups

--- hyperalignment/analysis.py
+++ hyperalignment/analysis.py
@@ -33,30 +33,47 @@ def cka(x1, x2):
 
 def plot_side_by_side(args):
     data = load_experiment_data(args)
-    mapper_weights = data["mapper_weights"]
-    cond_embs = data["cond_embs"]
+    group_size = int(args.experiment_name.split("_")[1]) // 3
 
-    setting_to_fn = {
-        "cosine_sim": cosine_sim,
-        "euc_dist": euc_dist,
-        "cka": cka
-    }
-    mapper_grid = setting_to_fn[args.mapper_metric](mapper_weights, mapper_weights)
-    cond_emb_grid = setting_to_fn[args.cond_emb_metric](cond_embs, cond_embs)
+    fig, axs = plt.subplots(3, 2, figsize=(10, 5))
+    for i in range(3):
+        start = i * group_size
+        end = i * group_size + group_size
 
-    mapper_grid = mapper_grid.cpu().numpy()
-    cond_emb_grid = cond_emb_grid.cpu().numpy()
+        mapper_weights = data["mapper_weights"][start : end]
 
-    fig, axs = plt.subplots(1, 2, figsize=(10, 5))
-    axs[0].imshow(mapper_grid.cpu().numpy())
-    axs[0].set_xlabel("Mapper index")
-    axs[0].set_ylabel("Mapper index")
-    axs[0].set_title(f"Intra Mapper {args.mapper_metric}")
+        print(len(mapper_weights))
+        c = 0
+        for idx, item in enumerate(mapper_weights):
+            if item[1].dim() == 1:
+                mapper_weights[idx] = torch.cat([item[0], item[1].unsqueeze(-1)])
+                c += 1
+        
+        print(c, mapper_weights.shape)
+        mapper_weights = torch.stack(mapper_weights)
+        cond_embs = data["cond_embs"][start : end, ...]
+        assert mapper_weights.shape[0] == cond_embs.shape[0]
 
-    axs[1].imshow(cond_emb_grid.cpu().numpy())
-    axs[1].set_xlabel("Conditional Embedding index")
-    axs[1].set_ylabel("Conditional Embedding index")
-    axs[1].set_title(f"Intra Conditional Embedding {args.cond_emb_metric}")
+        setting_to_fn = {
+            "cosine_sim": cosine_sim,
+            "euc_dist": euc_dist,
+            "cka": cka
+        }
+        mapper_grid = setting_to_fn[args.mapper_metric](mapper_weights, mapper_weights)
+        cond_emb_grid = setting_to_fn[args.cond_emb_metric](cond_embs, cond_embs)
+
+        mapper_grid = mapper_grid.cpu().numpy()
+        cond_emb_grid = cond_emb_grid.cpu().numpy()
+
+        axs[i, 0].imshow(mapper_grid.cpu().numpy())
+        axs[i, 0].set_xlabel("Mapper index")
+        axs[i, 0].set_ylabel("Mapper index")
+        axs[i, 0].set_title(f"Group {i+1}\nIntra Mapper {args.mapper_metric}")
+
+        axs[i, 1].imshow(cond_emb_grid.cpu().numpy())
+        axs[i, 1].set_xlabel("Conditional Embedding index")
+        axs[i, 1].set_ylabel("Conditional Embedding index")
+        axs[i, 1].set_title(f"Group {i+1}\nIntra Conditional Embedding {args.cond_emb_metric}")
 
     plt.tight_layout()
 
