62bca1927c3c147ef5e5f7b0e9f356c510e65d3a
jaisidhsingh <jaisidhsingh@gmail.com>
2024-12-29 22:54:29 +0100

    avg of 768+1024 dim ces

--- hyperalignment/run_ood.sh
+++ hyperalignment/run_ood.sh
@@ -1,14 +1,15 @@
-python3 ood_autodecoding.py \
-    --image-encoder="beit_base_patch16_224.in22k_ft_in22k_in1k" \
-    --image-embed-dim=768 \
-    --learning-rate=1e-2;
+# python3 ood_autodecoding.py \
+#     --image-encoder="beit_base_patch16_224.in22k_ft_in22k_in1k" \
+#     --image-embed-dim=768 \
+#     --learning-rate=1e-2;
 
 python3 evaluation.py \
     --exp-name="beit_base_patch16_224.in22k_ft_in22k_in1k" \
     --seed=0 \
     --run-type="ood" \
     --ood-results-path="ood_attempt_10k_avg_beit_768.pt" \
-    --epoch=10 \
+    --image-embed-dim=768 \
+    --epoch=1 \
     --encoder-index=5 \
     --benchmarks="cifar10,cifar100,imagenet1k,mscoco";
 # write test
