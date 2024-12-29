ba56670e7094d812684378a94add18f89fd06782
jaisidhsingh <jaisidhsingh@gmail.com>
2024-12-29 23:11:00 +0100

    check another 768

--- hyperalignment/run_ood.sh
+++ hyperalignment/run_ood.sh
@@ -1,17 +1,17 @@
 python3 ood_autodecoding.py \
-    --image-encoder="flexivit_small.300ep_in1k" \
-    --image-embed-dim=384 \
+    --image-encoder="deit3_base_patch16_224.fb_in22k_ft_in1k" \
+    --image-embed-dim=768 \
     --learning-rate=1e-4 \
     --save-path="ood_attempt_10k_avg_visf_384.pt" \
     --batch-size=8;
 
 python3 evaluation.py \
-    --exp-name="visformer_tiny.in1k" \
+    --exp-name="deit3_base_patch16.fb_in22k_ft_in1k" \
     --seed=0 \
     --run-type="ood" \
     --ood-results-path="ood_attempt_10k_avg_visf_384.pt" \
-    --image-embed-dim=384 \
+    --image-embed-dim=768 \
     --epoch=1 \
-    --encoder-index=6 \
+    --encoder-index=5 \
     --benchmarks="cifar10,cifar100,imagenet1k,mscoco";
 # write test
