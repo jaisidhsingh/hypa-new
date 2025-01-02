4cf9126443710ab803a4d6dc9e597b9be4930483
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-02 15:00:57 +0100

    ie 24 visformer 384 ood

--- hyperalignment/run_ood.sh
+++ hyperalignment/run_ood.sh
@@ -1,8 +1,8 @@
 python3 ood_autodecoding.py \
-    --image-encoder="efficientvit_m5.r224_in1k" \
+    --image-encoder="visformer_tiny.in1k" \
     --image-embed-dim=384 \
-    --hnet-ckpt-name="ie_30_mlp_c_32_norm" \
-    --hnet-ckpt-num-ie=30 \
+    --hnet-ckpt-name="ie_24_mlp_c_32_norm" \
+    --hnet-ckpt-num-ie=24 \
     --learning-rate=1e-4 \
     --save-path="x.pt" \
     --batch-size=8;
@@ -14,5 +14,5 @@ python3 evaluation.py \
     --ood-results-path="x.pt" \
     --image-embed-dim=768 \
     --epoch=1 \
-    --encoder-index=4 \
+    --encoder-index=6 \
     --benchmarks="cifar10,cifar100,mscoco,imagenet1k";
