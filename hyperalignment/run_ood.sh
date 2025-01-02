0145f4f932610959feb0548031201675a2c01831
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-02 15:21:50 +0100

    ood on ie_9 deit3 384

--- hyperalignment/run_ood.sh
+++ hyperalignment/run_ood.sh
@@ -1,18 +1,18 @@
 python3 ood_autodecoding.py \
-    --image-encoder="visformer_tiny.in1k" \
+    --image-encoder="diet3_small_patch16_224.fb_in22k_ft_in1k" \
     --image-embed-dim=384 \
-    --hnet-ckpt-name="ie_18_mlp_c_32_norm" \
-    --hnet-ckpt-num-ie=18 \
+    --hnet-ckpt-name="ie_9_mlp_c_32_norm" \
+    --hnet-ckpt-num-ie=9 \
     --learning-rate=1e-4 \
     --save-path="x.pt" \
     --batch-size=8;
 
-python3 evaluation.py \
-    --exp-name="efficientvit_m5.r224_in1k" \
-    --seed=0 \
-    --run-type="ood" \
-    --ood-results-path="x.pt" \
-    --image-embed-dim=768 \
-    --epoch=1 \
-    --encoder-index=6 \
-    --benchmarks="cifar10,cifar100,mscoco,imagenet1k";
+# python3 evaluation.py \
+#     --exp-name="efficientvit_m5.r224_in1k" \
+#     --seed=0 \
+#     --run-type="ood" \
+#     --ood-results-path="x.pt" \
+#     --image-embed-dim=768 \
+#     --epoch=1 \
+#     --encoder-index=6 \
+#     --benchmarks="cifar10,cifar100,mscoco,imagenet1k";
