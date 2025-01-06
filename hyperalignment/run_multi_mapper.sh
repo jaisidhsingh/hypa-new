e0e1eb13d9d91ed0f90c92bb3aea176048922d9a
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-06 22:12:15 +0100

    test out new mm

--- hyperalignment/run_multi_mapper.sh
+++ hyperalignment/run_multi_mapper.sh
@@ -1,8 +1,8 @@
 # c=32, mlp decoder, output norm, no init scaling, no scheduler
-# num_ie = 9
+# num_ie = 30
 python3 src/learn_multi_mapper.py \
-    --experiment-name="ie_9_mlp_c_32_norm" \
-    --num-image-encoders=9 \
+    --experiment-name="ie_30_mlp_c_32_norm" \
+    --num-image-encoders=30 \
     --hnet-cond-emb-dim=32 \
     --scheduler="off" \
     --normalize-output=True \
@@ -10,67 +10,11 @@ python3 src/learn_multi_mapper.py \
 
 
 # c=32, attention decoder, output norm, no init scaling, no scheduler
-# num_ie = 9
+# num_ie = 30
 python3 src/learn_multi_mapper.py \
-    --experiment-name="ie_9_attention_c_32_norm" \
-    --num-image-encoders=9 \
+    --experiment-name="ie_30_attention_c_32_norm" \
+    --num-image-encoders=30 \
     --hnet-cond-emb-dim=32 \
     --scheduler="off" \
     --normalize-output=True \
     --hnet-decoder-type="attention";
-
-
-# # c=32, mlp decoder, output norm, no init scaling, no scheduler
-# # num_ie = 12
-# python3 src/learn_multi_mapper.py \
-#     --experiment-name="ie_12_mlp_c_32_norm" \
-#     --num-image-encoders=12 \
-#     --hidden-layer-factors="4,16" \
-#     --hnet-cond-emb-dim=32 \
-#     --scheduler="off" \
-#     --normalize-output=True \
-#     --rescale-factor=0.0;
-
-# # c=32, mlp decoder, output norm, no init scaling, no scheduler
-# # num_ie = 15
-# python3 src/learn_multi_mapper.py \
-#     --experiment-name="ie_15_mlp_c_32_norm" \
-#     --num-image-encoders=15 \
-#     --hidden-layer-factors="4,16" \
-#     --hnet-cond-emb-dim=32 \
-#     --scheduler="off" \
-#     --normalize-output=True \
-#     --rescale-factor=0.0;
-
-# # c=32, mlp decoder, output norm, no init scaling, no scheduler
-# # num_ie = 18
-# python3 src/learn_multi_mapper.py \
-#     --experiment-name="ie_18_mlp_c_32_norm" \
-#     --num-image-encoders=18 \
-#     --hidden-layer-factors="4,16" \
-#     --hnet-cond-emb-dim=32 \
-#     --scheduler="off" \
-#     --normalize-output=True \
-#     --rescale-factor=0.0;
-
-# # c=32, mlp decoder, output norm, no init scaling, no scheduler
-# # num_ie = 24
-# python3 src/learn_multi_mapper.py \
-#     --experiment-name="ie_24_mlp_c_32_norm" \
-#     --num-image-encoders=24 \
-#     --hidden-layer-factors="4,16" \
-#     --hnet-cond-emb-dim=32 \
-#     --scheduler="off" \
-#     --normalize-output=True \
-#     --rescale-factor=0.0;
-
-# # c=32, mlp decoder, output norm, no init scaling, no scheduler
-# # num_ie = 30
-# python3 src/learn_multi_mapper.py \
-#     --experiment-name="ie_30_mlp_c_32_norm" \
-#     --num-image-encoders=30 \
-#     --hidden-layer-factors="4,16" \
-#     --hnet-cond-emb-dim=32 \
-#     --scheduler="off" \
-#     --normalize-output=True \
-#     --rescale-factor=0.0;
