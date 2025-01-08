ef44bc7601b234b805d00330aa9416702ce7aa59
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-08 16:56:51 +0100

    run ood for dump

--- hyperalignment/run_eval.sh
+++ hyperalignment/run_eval.sh
@@ -1,10 +1,11 @@
-# python3 src/evaluation.py \
-#     --exp-name="vit_small_patch16_224" \
-#     --seed=0 \
-#     --run-type="sep" \
-#     --epoch=10 \
-#     --encoder-index=0 \
-#     --benchmarks="cifar10,cifar100,imagenet1k";
+python3 src/evaluation.py \
+    --exp-name="flexivit_small.300ep_in1k" \
+    --seed=0 \
+    --run-type="sep" \
+    --epoch=10 \
+    --encoder-index=0 \
+    --image-embed-dim=384 \
+    --benchmarks="cifar10,cifar100,imagenet1k";
 
 # python3 src/evaluation.py \
 #     --exp-name="vit_base_patch16_224" \
