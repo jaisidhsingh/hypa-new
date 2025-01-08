8b28724a6a42ffc5974190f764bdd764fbab4e71
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-08 18:19:01 +0100

    test ec mm on ood

--- hyperalignment/plots/ood_perf_v_flops.py
+++ hyperalignment/plots/ood_perf_v_flops.py
@@ -9,13 +9,5 @@ bars[1].set_color("tab:orange")
 
 plt.ylabel("ImageNet-1k Top-1 Accuracy (%)")
 plt.xlabel("Method")
-plt.show()
-
-
-eff_names = ["H-Net mini", "H-Net full"]
-eff_values = [20.0 , 1.73]
-ebars = plt.bar(eff_names, eff_values, width=0.6)
-bars[0].set_color("tab:purple")
-plt.ylabel("Efficiency in OOD over APE")
-plt.xlabel("Method")
-plt.show()
+plt.title("Our OOD opt. v/s APE (at 31.2 GFLOPs budget for both)")
+plt.savefig("results/ood_flexivit_ape_sb.png")
--- hyperalignment/run_ood.sh
+++ hyperalignment/run_ood.sh
@@ -1,7 +1,7 @@
 python3 src/ood_autodecoding.py \
     --image-encoder="flexivit_small.300ep_in1k" \
     --image-embed-dim=384 \
-    --hnet-ckpt-name="ie_12_mlp_c_32_norm" \
+    --hnet-ckpt-name="ie_12_mlp_c_32_norm_ec" \
     --hnet-ckpt-num-ie=12 \
     --learning-rate=1e-5 \
     --save-path="x.pt" \
