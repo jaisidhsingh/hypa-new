a53cecebf0a68defb06110674deaac01195fc863
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-08 14:29:28 +0100

    add clip to bars

--- hyperalignment/plots/image_clf_plots.py
+++ hyperalignment/plots/image_clf_plots.py
@@ -17,9 +17,10 @@ fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 5))
 width = 0.35
 x = np.arange(len(models))
 
-def create_bar_plot(ax, ape, hnet, title):
+def create_bar_plot(ax, ape, hnet, clip, title):
     ax.bar(x - width/2, hnet, width, label='H-Net')
     ax.bar(x + width/2, ape, width, label='APE')
+    ax.axhline(clip, linestyle="--", c="black", label="CLIP ViT-L/14")
     ax.set_ylabel('Accuracy (%)')
     ax.set_title(title)
     ax.set_xticks(x)
@@ -27,9 +28,9 @@ def create_bar_plot(ax, ape, hnet, title):
     ax.set_ylim([0, 100])
     ax.legend(loc="lower right")
 
-create_bar_plot(ax1, ape_cifar10, hnet_cifar10, 'CIFAR-10')
-create_bar_plot(ax2, ape_cifar100, hnet_cifar100, 'CIFAR-100')
-create_bar_plot(ax3, ape_imagenet, hnet_imagenet, 'ImageNet-1k')
+create_bar_plot(ax1, ape_cifar10, hnet_cifar10, 91.40, 'CIFAR-10')
+create_bar_plot(ax2, ape_cifar100, hnet_cifar100, 65.95, 'CIFAR-100')
+create_bar_plot(ax3, ape_imagenet, hnet_imagenet, 75.51, 'ImageNet-1k')
 
 plt.tight_layout()
 plt.savefig("small_scale_vits_img_clf.png")
