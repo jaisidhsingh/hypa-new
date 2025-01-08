a5488d7f66dbbf60dedac586037599c779330d1f
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-08 15:24:47 +0100

    run ood for dump

--- hyperalignment/run_multi_mapper.sh
+++ hyperalignment/run_multi_mapper.sh
@@ -31,13 +31,13 @@ python3 src/learn_multi_mapper.py \
 #     --hnet-decoder-type="mlp";
 
 
-# c=32, mlp decoder, output norm, no init scaling, no scheduler
-# num_ie = 30
-python3 src/learn_multi_mapper.py \
-    --experiment-name="ie_30_mlp_c_32_norm" \
-    --num-image-encoders=30 \
-    --hnet-cond-emb-dim=32 \
-    --scheduler="off" \
-    --normalize-output=True \
-    --hnet-decoder-type="mlp"\
-    --num-epochs=20;
+# # c=32, mlp decoder, output norm, no init scaling, no scheduler
+# # num_ie = 30
+# python3 src/learn_multi_mapper.py \
+#     --experiment-name="ie_30_mlp_c_32_norm" \
+#     --num-image-encoders=30 \
+#     --hnet-cond-emb-dim=32 \
+#     --scheduler="off" \
+#     --normalize-output=True \
+#     --hnet-decoder-type="mlp"\
+#     --num-epochs=1;
