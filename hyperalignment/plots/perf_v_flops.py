448d99bd8287b62d0239148fde941c68956bcba6
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-08 12:08:29 +0100

    new plot+ run hnet for more eps

--- hyperalignment/plots/perf_v_flops.py
+++ hyperalignment/plots/perf_v_flops.py
@@ -1,6 +1,25 @@
 from pylab import plt
 
-ape_accs = [39.95]
+ape_accs = [12.00, 38.40, 39.95]
+ape_epochs = [1, 5, 10]
+ape_flops = [2250.56e+9 * 37 * i for i in ape_epochs]
+
 hnet_acc = [31.45]
+hnet_epochs = [1]
 hnet_flops = [73151e+9]
 
+plt.plot(hnet_flops, hnet_acc, marker="*", label="H-Net ViT-L/16")
+plt.plot(ape_flops, ape_accs, marker="o", label="APE ViT-L/16")
+plt.xlabel("Trainable FLOPs")
+# plt.xscale("log")
+plt.ylabel("ImageNet-1k Top-1 Accuracy")
+plt.ylim([0, 100])
+plt.axhline(75.51, linestyle="--", c="black", label="CLIP ViT-L/14")
+plt.legend()
+
+for i in range(3):
+    plt.annotate(str(ape_epochs[i]), (ape_flops[i], ape_accs[i]))
+
+plt.annotate(1, (hnet_flops[0], hnet_acc[0]))
+
+plt.savefig("vitl_imagenet_flops.png")
--- hyperalignment/run_multi_mapper.sh
+++ hyperalignment/run_multi_mapper.sh
@@ -1,34 +1,34 @@
-# c=32, mlp decoder, output norm, no init scaling, no scheduler
-# num_ie = 12
-python3 src/learn_multi_mapper.py \
-    --experiment-name="ie_12_mlp_c_32_norm" \
-    --num-image-encoders=12 \
-    --hnet-cond-emb-dim=32 \
-    --scheduler="off" \
-    --normalize-output=True \
-    --hnet-decoder-type="mlp";
+# # c=32, mlp decoder, output norm, no init scaling, no scheduler
+# # num_ie = 12
+# python3 src/learn_multi_mapper.py \
+#     --experiment-name="ie_12_mlp_c_32_norm" \
+#     --num-image-encoders=12 \
+#     --hnet-cond-emb-dim=32 \
+#     --scheduler="off" \
+#     --normalize-output=True \
+#     --hnet-decoder-type="mlp";
 
 
-# c=32, mlp decoder, output norm, no init scaling, no scheduler
-# num_ie = 18
-python3 src/learn_multi_mapper.py \
-    --experiment-name="ie_18_mlp_c_32_norm" \
-    --num-image-encoders=18 \
-    --hnet-cond-emb-dim=32 \
-    --scheduler="off" \
-    --normalize-output=True \
-    --hnet-decoder-type="mlp";
+# # c=32, mlp decoder, output norm, no init scaling, no scheduler
+# # num_ie = 18
+# python3 src/learn_multi_mapper.py \
+#     --experiment-name="ie_18_mlp_c_32_norm" \
+#     --num-image-encoders=18 \
+#     --hnet-cond-emb-dim=32 \
+#     --scheduler="off" \
+#     --normalize-output=True \
+#     --hnet-decoder-type="mlp";
 
 
-# c=32, mlp decoder, output norm, no init scaling, no scheduler
-# num_ie = 24
-python3 src/learn_multi_mapper.py \
-    --experiment-name="ie_24_mlp_c_32_norm" \
-    --num-image-encoders=24 \
-    --hnet-cond-emb-dim=32 \
-    --scheduler="off" \
-    --normalize-output=True \
-    --hnet-decoder-type="mlp";
+# # c=32, mlp decoder, output norm, no init scaling, no scheduler
+# # num_ie = 24
+# python3 src/learn_multi_mapper.py \
+#     --experiment-name="ie_24_mlp_c_32_norm" \
+#     --num-image-encoders=24 \
+#     --hnet-cond-emb-dim=32 \
+#     --scheduler="off" \
+#     --normalize-output=True \
+#     --hnet-decoder-type="mlp";
 
 
 # c=32, mlp decoder, output norm, no init scaling, no scheduler
@@ -39,4 +39,5 @@ python3 src/learn_multi_mapper.py \
     --hnet-cond-emb-dim=32 \
     --scheduler="off" \
     --normalize-output=True \
-    --hnet-decoder-type="mlp";
+    --hnet-decoder-type="mlp"\
+    --num-epochs=20;
