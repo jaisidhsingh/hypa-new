de131490cb83b3c43d3a489d337c145fbbfe1f7a
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-08 19:12:42 +0100

    log off commit

--- hyperalignment/plots/ood_perf_v_flops.py
+++ hyperalignment/plots/ood_perf_v_flops.py
@@ -1,7 +1,7 @@
 from pylab import plt
 
 models = ["H-Net OOD", "APE"]
-values = [0.12, 2.29]
+values = [0.12, 2.16]
 
 bars = plt.bar(models, values, width=0.4)
 bars[0].set_color("tab:purple")
@@ -10,4 +10,4 @@ bars[1].set_color("tab:orange")
 plt.ylabel("ImageNet-1k Top-1 Accuracy (%)")
 plt.xlabel("Method")
 plt.title("Our OOD opt. v/s APE (at 31.2 GFLOPs budget for both)")
-plt.savefig("results/ood_flexivit_ape_sb.png")
+plt.savefig("results/ood_visformer_ape_sb.png")
