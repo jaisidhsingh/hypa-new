f098c7aeff7a0d0e866cdb0235f948eb03cc1b8c
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-08 15:40:06 +0100

    run ood for dump

--- hyperalignment/run_ape.sh
+++ hyperalignment/run_ape.sh
@@ -1,19 +1,19 @@
 python3 src/learn_separate_mapping.py \
-    --experiment-name="vit_small_patch16_224" \
-    --image-encoder="vit_small_patch16_224" \
+    --experiment-name="flexivit_small.300ep_in1k" \
+    --image-encoder="flexivit_small.300ep_in1k" \
     --image-embed-dim=384 \
     --num-epochs=20;
 
 
-python3 src/learn_separate_mapping.py \
-    --experiment-name="vit_base_patch16_224" \
-    --image-encoder="vit_base_patch16_224" \
-    --image-embed-dim=768 \
-    --num-epochs=20;
+# python3 src/learn_separate_mapping.py \
+#     --experiment-name="vit_base_patch16_224" \
+#     --image-encoder="vit_base_patch16_224" \
+#     --image-embed-dim=768 \
+#     --num-epochs=20;
 
 
-python3 src/learn_separate_mapping.py \
-    --experiment-name="vit_large_patch16_224" \
-    --image-encoder="vit_large_patch16_224" \
-    --image-embed-dim=1024 \
-    --num-epochs=20;
+# python3 src/learn_separate_mapping.py \
+#     --experiment-name="vit_large_patch16_224" \
+#     --image-encoder="vit_large_patch16_224" \
+#     --image-embed-dim=1024 \
+#     --num-epochs=20;
