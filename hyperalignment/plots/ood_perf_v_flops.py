58faafc13c8f7348b60d6d450e11756eca5a51e8
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-08 17:21:02 +0100

    full ood opt

--- hyperalignment/plots/ood_perf_v_flops.py
+++ hyperalignment/plots/ood_perf_v_flops.py
@@ -1,9 +1,12 @@
 from pylab import plt
 
 models = ["H-Net mini", "H-Net full", "APE full"]
-values = [0.0, 25.8, 23.8]
+values = [23.0, 25.8, 23.8]
 
-plt.bar(models, values)
+bars = plt.bar(models, values, width=0.4)
+bars[0].set_color("tab:purple")
+bars[2].set_color("tab:orange")
+
 plt.ylabel("ImageNet-1k Top-1 Accuracy (%)")
 plt.xlabel("Methods")
 plt.show()
