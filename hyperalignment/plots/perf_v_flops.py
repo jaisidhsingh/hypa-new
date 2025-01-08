6372cf8c54d23ff22042937c646aea8d44c3152a
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-08 15:22:05 +0100

    run ood for dump

--- hyperalignment/plots/perf_v_flops.py
+++ hyperalignment/plots/perf_v_flops.py
@@ -4,9 +4,9 @@ ape_accs = [12.00, 38.40, 39.95]
 ape_epochs = [1, 5, 10]
 ape_flops = [2250.56e+9 * 37 * i for i in ape_epochs]
 
-hnet_acc = [31.45, 36.10]
-hnet_epochs = [1, 5]
-hnet_flops = [73151e+9 * i for i in hnet_epochs]
+hnet_acc = [31.45] #, 36.10]
+hnet_epochs = [1] #, 5]
+hnet_flops = [73151e+9]  #* i for i in hnet_epochs]
 
 plt.plot(hnet_flops, hnet_acc, marker="*", label="H-Net ViT-L/16")
 plt.plot(ape_flops, ape_accs, marker="o", label="APE ViT-L/16")
@@ -18,8 +18,9 @@ plt.axhline(75.51, linestyle="--", c="black", label="CLIP ViT-L/14")
 plt.legend()
 
 for i in range(3):
-    plt.annotate(str(ape_epochs[i]), (ape_flops[i], ape_accs[i]))
+    plt.annotate(f"Epoch {ape_epochs[i]}", (ape_flops[i], ape_accs[i] + 3))
 
-plt.annotate(1, (hnet_flops[0], hnet_acc[0]))
+plt.annotate(f"Epoch 1", (hnet_flops[0], hnet_acc[0] + 3))
 
+plt.title("ViT-L/16 Performance v/s FLOPs tradeoff")
 plt.savefig("vitl_imagenet_flops.png")
