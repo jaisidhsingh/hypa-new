9208170a0f835635cbf202470b59d7ff78fb35b8
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-13 12:21:07 +0100

    use features as cond_ids

--- hyperalignment/run_multi_mapper.sh
+++ hyperalignment/run_multi_mapper.sh
@@ -1,12 +1,13 @@
 # c=32, mlp decoder, output norm, no init scaling, no scheduler
 # num_ie = 12
 python3 src/learn_multi_mapper.py \
-    --experiment-name="ie_12_mlp_c_32_norm_ec" \
+    --experiment-name="ie_12_mlp_c_32_norm_ft" \
     --num-image-encoders=12 \
     --hnet-cond-emb-dim=32 \
     --scheduler="off" \
     --normalize-output=True \
-    --emb-loss=True \
+    --emb-loss=False \
+    --cond-type="features" \
     --hnet-decoder-type="mlp";
 
 
