22902b1ba372ea97657e16b51241e0f2ebf4b364
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-13 13:02:36 +0100

    eval ood for perm

--- hyperalignment/run_eval.sh
+++ hyperalignment/run_eval.sh
@@ -7,14 +7,14 @@
 #     --image-embed-dim=384 \
 #     --benchmarks="cifar10,cifar100,imagenet1k";
 
-python3 src/evaluation.py \
-    --exp-name="vit_small_patch16_224" \
-    --seed=0 \
-    --run-type="sep" \
-    --epoch=1 \
-    --image-embed-dim=384 \
-    --encoder-index=0 \
-    --benchmarks="cifar10,cifar100,imagenet1k";
+# python3 src/evaluation.py \
+#     --exp-name="vit_small_patch16_224" \
+#     --seed=0 \
+#     --run-type="sep" \
+#     --epoch=1 \
+#     --image-embed-dim=384 \
+#     --encoder-index=0 \
+#     --benchmarks="cifar10,cifar100,imagenet1k";
 
 # python3 src/evaluation.py \
 #     --exp-name="vit_large_patch16_224" \
@@ -25,14 +25,14 @@ python3 src/evaluation.py \
 #     --encoder-index=0 \
 #     --benchmarks="imagenet1k";
 
-python3 src/evaluation.py \
-    --exp-name="ie_12_mlp_c_32_norm_ft" \
-    --seed=0 \
-    --run-type="mm" \
-    --epoch=1 \
-    --encoder-index=0 \
-    --image-embed-dim=384 \
-    --benchmarks="cifar10,cifar100,imagenet1k";
+# python3 src/evaluation.py \
+#     --exp-name="ie_12_mlp_c_32_norm_ft" \
+#     --seed=0 \
+#     --run-type="mm" \
+#     --epoch=1 \
+#     --encoder-index=0 \
+#     --image-embed-dim=384 \
+#     --benchmarks="cifar10,cifar100,imagenet1k";
 
 # python3 src/evaluation.py \
 #     --exp-name="ie_30_mlp_c_32_norm" \
@@ -52,13 +52,13 @@ python3 src/evaluation.py \
 #     --image-embed-dim=1024 \
 #     --benchmarks="imagenet1k";
 
-# python3 src/evaluation.py \
-#     --exp-name="visformer_tiny.in1k" \
-#     --seed=0 \
-#     --run-type="ood" \
-#     --ood-results-path="x.pt" \
-#     --epoch=10 \
-#     --encoder-index=6 \
-#     --image-embed-dim=384 \
-#     --benchmarks="cifar10,cifar100,imagenet1k";
+python3 src/evaluation.py \
+    --exp-name="visformer_tiny.in1k" \
+    --seed=0 \
+    --run-type="ood" \
+    --ood-results-path="x.pt" \
+    --epoch=10 \
+    --encoder-index=6 \
+    --image-embed-dim=384 \
+    --benchmarks="cifar10,cifar100,imagenet1k";
 
