0f582d7301fd167c03a08f93a5e078c7fcbd916a
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-15 13:53:52 +0100

    x

--- hyperalignment/run_id_training.sh
+++ hyperalignment/run_id_training.sh
@@ -57,7 +57,7 @@ python3 src/learn_hnet.py \
     --hnet-cond-emb-dim=32 \
     --cond-type="features" \
     --num-epochs=1 \
-    --learning-rate=1e-1 \
+    --learning-rate=1e-2 \
     --scheduler="off" \
     --warmup-steps=100 \
     --batch-size=512 \
