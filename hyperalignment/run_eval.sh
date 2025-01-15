35289ed1acba2383509b3dd2f1fcf18bb5299abf
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-15 09:13:57 +0100

    run ep 1,5 for hnet-ft vits

--- hyperalignment/run_eval.sh
+++ hyperalignment/run_eval.sh
@@ -1,11 +1,11 @@
-python3 src/evaluation.py \
-    --exp-name="flexivit_small.300ep_in1k" \
-    --seed=0 \
-    --run-type="sep" \
-    --epoch=1 \
-    --encoder-index=5 \
-    --image-embed-dim=384 \
-    --benchmarks="cifar10,cifar100,imagenet1k";
+# python3 src/evaluation.py \
+#     --exp-name="flexivit_small.300ep_in1k" \
+#     --seed=0 \
+#     --run-type="sep" \
+#     --epoch=1 \
+#     --encoder-index=5 \
+#     --image-embed-dim=384 \
+#     --benchmarks="cifar10,cifar100,imagenet1k";
 
 # python3 src/evaluation.py \
 #     --exp-name="vit_small_patch16_224" \
@@ -25,14 +25,23 @@ python3 src/evaluation.py \
 #     --encoder-index=0 \
 #     --benchmarks="imagenet1k";
 
-# python3 src/evaluation.py \
-#     --exp-name="ie_12_mlp_c_32_norm_ft" \
-#     --seed=0 \
-#     --run-type="mm" \
-#     --epoch=1 \
-#     --encoder-index=0 \
-#     --image-embed-dim=384 \
-#     --benchmarks="cifar10,cifar100,imagenet1k";
+python3 src/evaluation.py \
+    --exp-name="ie_12_mlp_c_32_norm_ft" \
+    --seed=0 \
+    --run-type="mm" \
+    --epoch=5 \
+    --encoder-index=0 \
+    --image-embed-dim=384 \
+    --benchmarks="cifar10,cifar100,imagenet1k";
+
+python3 src/evaluation.py \
+    --exp-name="ie_12_mlp_c_32_norm_ft" \
+    --seed=0 \
+    --run-type="mm" \
+    --epoch=10 \
+    --encoder-index=0 \
+    --image-embed-dim=384 \
+    --benchmarks="cifar10,cifar100,imagenet1k";
 
 # python3 src/evaluation.py \
 #     --exp-name="ie_30_mlp_c_32_norm" \
