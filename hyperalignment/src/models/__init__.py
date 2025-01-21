20c1d19646953f6b4f8ab4d93dcfc2a64143eb7c
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-21 19:56:16 +0100

    chunked hnet

--- hyperalignment/src/models/__init__.py
+++ hyperalignment/src/models/__init__.py
@@ -152,7 +152,7 @@ class ConditionalHyperNetwork(nn.Module):
         
         mapped_text_features = self.map_features(pred_weight, pred_bias, text_features)
         mapped_text_features = mapped_text_features / mapped_text_features.norm(dim=-1, keepdim=True)
-        loss, corrects = self.compute_loss(self.logit_scale.to(text_features.device), image_features, text_features)
+        loss, corrects = self.compute_loss(self.logit_scale.to(mapped_text_features.device), image_features, mapped_text_features)
 
 
     def map_features(self, weights, biases, features):
