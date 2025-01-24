824bc401c8a720e61489292f811c0c6667bc0e93
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-24 15:37:42 +0100

    chunked ood

--- hyperalignment/run_ood.sh
+++ hyperalignment/run_ood.sh
@@ -11,7 +11,7 @@
 python3 src/ood_autodecoding.py \
     --image-encoder="visformer_tiny.in1k" \
     --image-embed-dim=384 \
-    --hnet-ckpt-name="ie_12_mlp_c_32_norm" \
+    --hnet-ckpt-name="ie_12-4_mlp_c-32_norm_chk-256_inproj" \
     --hnet-ckpt-num-ie=12 \
     --learning-rate=1e-5 \
     --save-path="x.pt" \
@@ -20,7 +20,7 @@ python3 src/ood_autodecoding.py \
 python3 src/ood_autodecoding.py \
     --image-encoder="volo_d1_224.sail_in1k" \
     --image-embed-dim=384 \
-    --hnet-ckpt-name="ie_12_mlp_c_32_norm" \
+    --hnet-ckpt-name="ie_12-4_mlp_c-32_norm_chk-256_inproj" \
     --hnet-ckpt-num-ie=12 \
     --learning-rate=1e-5 \
     --save-path="x.pt" \
