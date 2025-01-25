907e8ac9d37cfe56cfa76713e81e51d2b4345161
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-25 11:11:00 +0100

    ape to 20 eps

--- hyperalignment/src/embed_data.py
+++ hyperalignment/src/embed_data.py
@@ -93,7 +93,8 @@ def main():
     # one_encoder_embeds_images(args)
     
     args.text_embed_dim = 384
-    args.text_encoder = "all-MiniLM-L12-v2"
+    # args.text_encoder = "all-MiniLM-L12-v2"
+    args.text_encoder = "all-mpnet-base-v1"
     args.text_results_folder = "/network/scratch/s/sparsha.mishra/hyperalignment/results/text_embeddings/icml"
     one_encoder_embeds_texts(args)
 
