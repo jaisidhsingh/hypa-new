839b7dde4259bcbb10ef90345cc1fdd5dea7ffa8
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-25 16:24:41 +0100

    full mlp hnet-bs

--- hyperalignment/run_tmp.sh
+++ hyperalignment/run_tmp.sh
@@ -1,26 +1,47 @@
-# python3 src/evaluation.py \
-#     --exp-name="hnet_12-4_mlp_c-32_bs-512_lr-1e-2" \
-#     --seed=0 \
-#     --run-type="mm" \
-#     --epoch=20 \
-#     --encoder-index=0 \
-#     --image-embed-dim=384 \
-#     --benchmarks="imagenet1k";
+python3 src/learn_hnet.py \
+    --experiment-name="hnet_12-4_fmlp_c-32_bs-1024_lr-2e-2" \
+    --num-image-encoders=12 \
+    --encoder-batch-size=4 \
+    --hnet-cond-emb-dim=32 \
+    --cond-type="features" \
+    --num-epochs=20 \
+    --learning-rate=2e-2 \
+    --batch-size=1024 \
+    --scheduler="off" \
+    --warmup-steps=100 \
+    --emb-loss=False \
+    --normalize-output=True \
+    --chunk-dim=256 \
+    --hnet-decoder-type="mlp";
 
-python3 src/evaluation.py \
-    --exp-name="hnet_12-4_chkmlp_c-32_bs-4096-lr-3e-2" \
-    --seed=0 \
-    --run-type="mm" \
-    --epoch=20 \
-    --encoder-index=0 \
-    --image-embed-dim=384 \
-    --benchmarks="imagenet1k";
+python3 src/learn_hnet.py \
+    --experiment-name="hnet_12-4_fmlp_c-32_bs-4096_lr-3e-2" \
+    --num-image-encoders=12 \
+    --encoder-batch-size=4 \
+    --hnet-cond-emb-dim=32 \
+    --cond-type="features" \
+    --num-epochs=20 \
+    --learning-rate=3e-2 \
+    --batch-size=4096 \
+    --scheduler="off" \
+    --warmup-steps=100 \
+    --emb-loss=False \
+    --normalize-output=True \
+    --chunk-dim=256 \
+    --hnet-decoder-type="mlp";
 
-python3 src/evaluation.py \
-    --exp-name="hnet_12-4_chkmlp_c-32_bs-16384-lr-5e-2" \
-    --seed=0 \
-    --run-type="mm" \
-    --epoch=20 \
-    --encoder-index=0 \
-    --image-embed-dim=384 \
-    --benchmarks="imagenet1k";
+python3 src/learn_hnet.py \
+    --experiment-name="hnet_12-4_fmlp_c-32_bs-16384_lr-5e-2" \
+    --num-image-encoders=12 \
+    --encoder-batch-size=4 \
+    --hnet-cond-emb-dim=32 \
+    --cond-type="features" \
+    --num-epochs=20 \
+    --learning-rate=5e-2 \
+    --batch-size=16384 \
+    --scheduler="off" \
+    --warmup-steps=100 \
+    --emb-loss=False \
+    --normalize-output=True \
+    --chunk-dim=256 \
+    --hnet-decoder-type="mlp";
