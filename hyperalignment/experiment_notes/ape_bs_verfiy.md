4be31b0f8073935c28911d2cce06e7bb02f44896
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-25 16:23:47 +0100

    in1k hnet-bs eval

--- hyperalignment/experiment_notes/ape_bs_verfiy.md
+++ hyperalignment/experiment_notes/ape_bs_verfiy.md
@@ -11,7 +11,7 @@
 - FLOPs for 20 epochs = 17.555 Trillion (877.7 Billion x 20)
 - ImageNet-1k top-1 accuracy = 25.45
 
-## 2. `bs=512` & `lr=1e-3` 
+## 2. `bs=512` & `lr=1e-3`
 
 - FLOPs for 20 epochs = 21.9 Trillion (1097.2 Billion x 20)
 - ImageNet-1k top-1 accuracy = 26.27
@@ -31,7 +31,7 @@
 - FLOPs for 20 epochs = 293.5 Trillion (14675.9 Billion x 20)
 - ImageNet-1k top-1 accuracy = 27.85
 
-# Chunked H-net
+# Chunked H-net (unswept)
 
 - Training dataset = cc595k (558k slice)
 - Num epochs = 20
@@ -40,9 +40,9 @@
 ## 1. `bs=256` & `lr=1e-3`
 
 - FLOPs for 20 epochs = 179.0 Trillion
-- ImageNet-1k top-1 accuracy = 18.2 
+- ImageNet-1k top-1 accuracy = 18.2
 
-## 2. `bs=512` & `lr=1e-3` 
+## 2. `bs=512` & `lr=1e-2`
 
 - FLOPs for 20 epochs = 238.7 Trillion
 - ImageNet-1k top-1 accuracy = 16.2
@@ -50,14 +50,14 @@
 ## 3. `bs=1024` & `lr=3e-3`
 
 - FLOPs for 20 epochs = 367.8 Trillion
-- ImageNet-1k top-1 accuracy = 17.3 
+- ImageNet-1k top-1 accuracy = 17.3
 
 ## 4. `bs=4096` & `le=5e-3`
 
 - FLOPs for 20 epochs = 1160.0 Trillion
-- ImageNet-1k top-1 accuracy = 
+- ImageNet-1k top-1 accuracy = 20.5
 
 ## 5. `bs=16384` & `lr=1e-2`
 
 - FLOPs for 20 epochs = 4336.6 Trillion
-- ImageNet-1k top-1 accuracy = 
+- ImageNet-1k top-1 accuracy = 10.9
--- hyperalignment/run_id_training.sh
+++ hyperalignment/run_id_training.sh
@@ -1,5 +1,5 @@
 python3 src/learn_hnet.py \
-    --experiment-name="hnet_12-4_chkmlp_c-32_bs-256_lr-3e-3" \
+    --experiment-name="hnet_12-4_fmlp_c-32_bs-256_lr-3e-3" \
     --num-image-encoders=12 \
     --encoder-batch-size=4 \
     --hnet-cond-emb-dim=32 \
@@ -12,10 +12,10 @@ python3 src/learn_hnet.py \
     --emb-loss=False \
     --normalize-output=True \
     --chunk-dim=256 \
-    --hnet-decoder-type="chunked_mlp";
+    --hnet-decoder-type="mlp";
 
 python3 src/learn_hnet.py \
-    --experiment-name="hnet_12-4_mlp_c-32_bs-512_lr-1e-2" \
+    --experiment-name="hnet_12-4_fmlp_c-32_bs-512_lr-1e-2" \
     --num-image-encoders=12 \
     --encoder-batch-size=4 \
     --hnet-cond-emb-dim=32 \
@@ -28,10 +28,10 @@ python3 src/learn_hnet.py \
     --emb-loss=False \
     --normalize-output=True \
     --chunk-dim=256 \
-    --hnet-decoder-type="chunked_mlp";
+    --hnet-decoder-type="mlp";
     
 python3 src/learn_hnet.py \
-    --experiment-name="hnet_12-4_chkmlp_c-32_bs-1024_lr-2e-2" \
+    --experiment-name="hnet_12-4_fmlp_c-32_bs-1024_lr-2e-2" \
     --num-image-encoders=12 \
     --encoder-batch-size=4 \
     --hnet-cond-emb-dim=32 \
@@ -44,10 +44,10 @@ python3 src/learn_hnet.py \
     --emb-loss=False \
     --normalize-output=True \
     --chunk-dim=256 \
-    --hnet-decoder-type="chunked_mlp";
+    --hnet-decoder-type="mlp";
 
 python3 src/learn_hnet.py \
-    --experiment-name="hnet_12-4_chkmlp_c-32_bs-4096-lr-3e-2" \
+    --experiment-name="hnet_12-4_fmlp_c-32_bs-4096_lr-3e-2" \
     --num-image-encoders=12 \
     --encoder-batch-size=4 \
     --hnet-cond-emb-dim=32 \
@@ -60,10 +60,10 @@ python3 src/learn_hnet.py \
     --emb-loss=False \
     --normalize-output=True \
     --chunk-dim=256 \
-    --hnet-decoder-type="chunked_mlp";
+    --hnet-decoder-type="mlp";
 
 python3 src/learn_hnet.py \
-    --experiment-name="hnet_12-4_chkmlp_c-32_bs-16384-lr-5e-2" \
+    --experiment-name="hnet_12-4_fmlp_c-32_bs-16384_lr-5e-2" \
     --num-image-encoders=12 \
     --encoder-batch-size=4 \
     --hnet-cond-emb-dim=32 \
@@ -76,4 +76,4 @@ python3 src/learn_hnet.py \
     --emb-loss=False \
     --normalize-output=True \
     --chunk-dim=256 \
-    --hnet-decoder-type="chunked_mlp";
+    --hnet-decoder-type="mlp";
