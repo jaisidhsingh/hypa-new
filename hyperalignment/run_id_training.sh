91ee70c28de7e5e6d0dc1f8bbffbfcac5d14bbf2
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-26 14:06:46 +0100

    hnet fmlp bs-abl reckpt 1-10

--- hyperalignment/run_id_training.sh
+++ hyperalignment/run_id_training.sh
@@ -1,5 +1,5 @@
 python3 src/learn_hnet.py \
-    --experiment-name="hnet_12-4_fmlp_c-32_bs-256_lr-3e-3" \
+    --experiment-name="hnet_12-4_fmlp_c-32_bs-1024_lr-2e-2" \
     --num-image-encoders=12 \
     --encoder-batch-size=4 \
     --hnet-cond-emb-dim=32 \
@@ -15,7 +15,7 @@ python3 src/learn_hnet.py \
     --hnet-decoder-type="mlp";
 
 python3 src/learn_hnet.py \
-    --experiment-name="hnet_12-4_fmlp_c-32_bs-512_lr-1e-2" \
+    --experiment-name="hnet_12-4_fmlp_c-32_bs-4096_lr-3e-2" \
     --num-image-encoders=12 \
     --encoder-batch-size=4 \
     --hnet-cond-emb-dim=32 \
