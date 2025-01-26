11145cb7817abd7d50a5c11c6a03319747b0464e
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-26 14:54:12 +0100

    figure out simple hnet

---
+++ hyperalignment/run_nmb.sh
@@ -0,0 +1,15 @@
+python3 src/learn_hnet.py \
+    --experiment-name="nmb_test_0" \
+    --num-image-encoders=6 \
+    --encoder-batch-size=6 \
+    --hnet-cond-emb-dim=32 \
+    --cond-type="features" \
+    --num-epochs=10 \
+    --learning-rate=3e-3 \
+    --batch-size=256 \
+    --scheduler="off" \
+    --warmup-steps=100 \
+    --emb-loss=False \
+    --normalize-output=True \
+    --chunk-dim=256 \
+    --hnet-decoder-type="mlp";
