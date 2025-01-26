72042fb2206cea1ca6ca3c7c0ef0df097ae2d6f1
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-26 15:35:34 +0100

    eval fmlp 512

---
+++ hyperalignment/plots/fmlp_bs.py
@@ -0,0 +1,32 @@
+
+data = {
+    'exp_name': 'hnet_12-4_fmlp_c-32_bs-256_lr-3e-3', 
+    'seed': 0, 
+    'eval': {
+        'epoch_1': {
+            'cifar10': 89.71, 
+            'cifar100': 45.1, 
+            'imagenet1k': 20.69
+        }, 
+        'epoch_2': {
+            'cifar10': 89.99, 
+            'cifar100': 47.73, 
+            'imagenet1k': 22.59
+        }, 
+        'epoch_5': {
+            'cifar10': 89.97, 
+            'cifar100': 49.08, 
+            'imagenet1k': 23.37
+        }, 
+        'epoch_10': {
+            'cifar10': 90.08, 
+            'cifar100': 49.23, 
+            'imagenet1k': 22.84
+        }, 
+        'epoch_20': {
+            'cifar10': 89.15, 
+            'cifar100': 47.93, 
+            'imagenet1k': 21.36
+        }
+    }
+}
