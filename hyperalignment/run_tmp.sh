526c7df887fd110f9f8a05e604fef22e30e9fa48
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-27 10:48:23 +0100

    sbatch fmlp hnet 100 eps

--- hyperalignment/run_tmp.sh
+++ hyperalignment/run_tmp.sh
@@ -16,26 +16,26 @@ conda activate /home/mila/s/sparsha.mishra/scratch/generate2
 
 ulimit -Sn $(ulimit -Hn)
 
-pyfile="/home/mila/s/sparsha.mishra/projects/hypa-new/hyperalignment/src/multi_ape.py"
+# pyfile="/home/mila/s/sparsha.mishra/projects/hypa-new/hyperalignment/src/learn_hnet.py"
 
-python3 $pyfile --num-workers=8;
+# python3 $pyfile --num-workers=8;
 
 
-# python3 src/learn_hnet.py \
-#     --experiment-name="hnet_12-4_fmlp_c-32_bs-1024_lr-2e-2" \
-#     --num-image-encoders=12 \
-#     --encoder-batch-size=4 \
-#     --hnet-cond-emb-dim=32 \
-#     --cond-type="features" \
-#     --num-epochs=20 \
-#     --learning-rate=2e-2 \
-#     --batch-size=1024 \
-#     --scheduler="off" \
-#     --warmup-steps=100 \
-#     --emb-loss=False \
-#     --normalize-output=True \
-#     --chunk-dim=256 \
-#     --hnet-decoder-type="mlp";
+python3 src/learn_hnet.py \
+    --experiment-name="hnet_12-4_fmlp_c-32_bs-512_lr-1e-2_ep-100" \
+    --num-image-encoders=12 \
+    --encoder-batch-size=4 \
+    --hnet-cond-emb-dim=32 \
+    --cond-type="features" \
+    --num-epochs=100 \
+    --learning-rate=1e-2 \
+    --batch-size=512 \
+    --scheduler="off" \
+    --warmup-steps=100 \
+    --emb-loss=False \
+    --normalize-output=True \
+    --chunk-dim=256 \
+    --hnet-decoder-type="mlp";
 
 # python3 src/learn_hnet.py \
 #     --experiment-name="hnet_12-4_fmlp_c-32_bs-4096_lr-3e-2" \
