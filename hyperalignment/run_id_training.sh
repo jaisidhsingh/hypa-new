62cf56d8a2a73b49c0c82650b29fbb1d4ae192ef
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-27 17:23:26 +0100

    sbatch ds_1-4

--- hyperalignment/run_id_training.sh
+++ hyperalignment/run_id_training.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=ds_1-2
+#SBATCH --job-name=ds_1-4
 #SBATCH --gres=gpu:rtx8000:1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -7,8 +7,8 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
 #SBATCH --partition=long
-#SBATCH --output=/home/mila/s/sparsha.mishra/projects/hypa-new/ds_1-2.out
-#SBATCH --error=/home/mila/s/sparsha.mishra/projects/hypa-new/ds_1-2.err
+#SBATCH --output=/home/mila/s/sparsha.mishra/projects/hypa-new/ds_1-4.out
+#SBATCH --error=/home/mila/s/sparsha.mishra/projects/hypa-new/ds_1-4.err
 
 module load anaconda/3
 
@@ -35,25 +35,8 @@ pyfile="/home/mila/s/sparsha.mishra/projects/hypa-new/hyperalignment/src/learn_h
     # --dataset-scale=0.75 \
     # --hnet-decoder-type="mlp";
 
-python3 $pyfile \
-    --experiment-name="half_hnet_12-4_fmlp_c-32_bs-512_lr-1e-2" \
-    --num-image-encoders=12 \
-    --encoder-batch-size=4 \
-    --hnet-cond-emb-dim=32 \
-    --cond-type="features" \
-    --num-epochs=20 \
-    --learning-rate=1e-2 \
-    --batch-size=512 \
-    --random-seed=0 \
-    --scheduler="off" \
-    --warmup-steps=100 \
-    --emb-loss=False \
-    --normalize-output=True \
-    --dataset-scale=0.5 \
-    --hnet-decoder-type="mlp";
-
 # python3 $pyfile \
-#     --experiment-name="one-fourth_hnet_12-4_fmlp_c-32_bs-512_lr-1e-2" \
+#     --experiment-name="half_hnet_12-4_fmlp_c-32_bs-512_lr-1e-2" \
 #     --num-image-encoders=12 \
 #     --encoder-batch-size=4 \
 #     --hnet-cond-emb-dim=32 \
@@ -66,8 +49,25 @@ python3 $pyfile \
 #     --warmup-steps=100 \
 #     --emb-loss=False \
 #     --normalize-output=True \
-#     --dataset-scale=0.25 \
+#     --dataset-scale=0.5 \
 #     --hnet-decoder-type="mlp";
+
+python3 $pyfile \
+    --experiment-name="one-fourth_hnet_12-4_fmlp_c-32_bs-512_lr-1e-2" \
+    --num-image-encoders=12 \
+    --encoder-batch-size=4 \
+    --hnet-cond-emb-dim=32 \
+    --cond-type="features" \
+    --num-epochs=20 \
+    --learning-rate=1e-2 \
+    --batch-size=512 \
+    --random-seed=0 \
+    --scheduler="off" \
+    --warmup-steps=100 \
+    --emb-loss=False \
+    --normalize-output=True \
+    --dataset-scale=0.25 \
+    --hnet-decoder-type="mlp";
 
 # python3 src/learn_hnet.py \
 #     --experiment-name="hnet_12-4_fmlp_c-32_bs-512_lr-1e-2" \
