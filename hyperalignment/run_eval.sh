f58c1e57e75338c99b26d63d8544abc8dcc2cfdf
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-27 15:33:57 +0100

    embed imagenet 384 fam

--- hyperalignment/run_eval.sh
+++ hyperalignment/run_eval.sh
@@ -7,24 +7,14 @@
 #     --image-embed-dim=384 \
 #     --benchmarks="cifar10,cifar100,imagenet1k";
 
-
-# python3 src/evaluation.py \
-#     --exp-name="flexivit_small.300ep_in1k" \
-#     --seed=0 \
-#     --run-type="sep" \
-#     --epoch=1 \
-#     --encoder-index=5 \
-#     --image-embed-dim=384 \
-#     --benchmarks="cifar10,cifar100,imagenet1k";
-
-# python3 src/evaluation.py \
-#     --exp-name="ie_12_mlp_c_32_norm" \
-#     --seed=0 \
-#     --run-type="mm" \
-#     --epoch=1 \
-#     --encoder-index=0 \
-#     --image-embed-dim=384 \
-#     --benchmarks="imagenet1k";
+python3 src/evaluation.py \
+    --exp-name="allMiniLM_hnet_12-4_fmlp_c-32_bs-512_lr-1e-2" \
+    --seed=0 \
+    --run-type="mm" \
+    --epoch=1 \
+    --encoder-index=0 \
+    --image-embed-dim=384 \
+    --benchmarks="cifar10,cifar100,imagenet1k";
 
 # python3 src/evaluation.py \
 #     --exp-name="flexivit_small.300ep_in1k" \
