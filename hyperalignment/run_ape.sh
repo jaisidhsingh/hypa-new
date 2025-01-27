1ca9557ac4093fbb30977b71c748892f4bd18776
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-27 16:32:02 +0100

    eval other tes

--- hyperalignment/run_ape.sh
+++ hyperalignment/run_ape.sh
@@ -1,15 +1,23 @@
-python3 src/train_ape.py \
-    --experiment-name="vit_small_patch16_224_all-MiniLM-L12-v2" \
-    --image-encoder="vit_small_patch16_224" \
-    --image-embed-dim=384 \
-    --num-epochs=20 \
-    --text-encoder="all-MiniLM-L12-v2" \
-    --text-embed-dim=384;
+# python3 src/train_ape.py \
+#     --experiment-name="vit_small_patch16_224_all-MiniLM-L12-v2" \
+#     --image-encoder="vit_small_patch16_224" \
+#     --image-embed-dim=384 \
+#     --num-epochs=20 \
+#     --text-encoder="all-MiniLM-L12-v2" \
+#     --text-embed-dim=384;
 
 python3 src/train_ape.py \
-    --experiment-name="vit_small_patch16_224_all-roberta-large-v1" \
+    --experiment-name="vit_small_patch16_224_sentence-t5-base" \
     --image-encoder="vit_small_patch16_224" \
     --image-embed-dim=384 \
     --num-epochs=20 \
-    --text-encoder="all-roberta-large-v1" \
-    --text-embed-dim=1024;
+    --text-encoder="sentence-t5-base" \
+    --text-embed-dim=768;
+
+# python3 src/train_ape.py \
+#     --experiment-name="vit_small_patch16_224_all-roberta-large-v1" \
+#     --image-encoder="vit_small_patch16_224" \
+#     --image-embed-dim=384 \
+#     --num-epochs=20 \
+#     --text-encoder="all-roberta-large-v1" \
+#     --text-embed-dim=1024;
