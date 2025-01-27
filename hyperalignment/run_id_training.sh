b4455ab028c2106ff29c19dfdfcbba25ff948a65
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-27 16:24:00 +0100

    dataset scale hnet

--- hyperalignment/run_id_training.sh
+++ hyperalignment/run_id_training.sh
@@ -1,5 +1,5 @@
 python3 src/learn_hnet.py \
-    --experiment-name="half_hnet_12-4_fmlp_c-32_bs-512_lr-1e-2" \
+    --experiment-name="three-fourth_hnet_12-4_fmlp_c-32_bs-512_lr-1e-2" \
     --num-image-encoders=12 \
     --encoder-batch-size=4 \
     --hnet-cond-emb-dim=32 \
@@ -13,6 +13,40 @@ python3 src/learn_hnet.py \
     --emb-loss=False \
     --normalize-output=True \
     --dataset-scale=0.75 \
+    --hnet-decoder-type="mlp";
+
+python3 src/learn_hnet.py \
+    --experiment-name="half_hnet_12-4_fmlp_c-32_bs-512_lr-1e-2" \
+    --num-image-encoders=12 \
+    --encoder-batch-size=4 \
+    --hnet-cond-emb-dim=32 \
+    --cond-type="features" \
+    --num-epochs=20 \
+    --learning-rate=1e-2 \
+    --batch-size=512 \
+    --random-seed=0 \
+    --scheduler="off" \
+    --warmup-steps=100 \
+    --emb-loss=False \
+    --normalize-output=True \
+    --dataset-scale=0.5 \
+    --hnet-decoder-type="mlp";
+
+python3 src/learn_hnet.py \
+    --experiment-name="one-fourth_hnet_12-4_fmlp_c-32_bs-512_lr-1e-2" \
+    --num-image-encoders=12 \
+    --encoder-batch-size=4 \
+    --hnet-cond-emb-dim=32 \
+    --cond-type="features" \
+    --num-epochs=20 \
+    --learning-rate=1e-2 \
+    --batch-size=512 \
+    --random-seed=0 \
+    --scheduler="off" \
+    --warmup-steps=100 \
+    --emb-loss=False \
+    --normalize-output=True \
+    --dataset-scale=0.25 \
     --hnet-decoder-type="mlp";
 
 # python3 src/learn_hnet.py \
