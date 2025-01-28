7e61870e1f567a69b66c30d2a9537e2e846f529b
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-28 20:02:34 +0100

    run 30_10

--- hyperalignment/run_id_training.sh
+++ hyperalignment/run_id_training.sh
@@ -19,22 +19,22 @@ ulimit -Sn $(ulimit -Hn)
 pyfile="/home/mila/s/sparsha.mishra/projects/hypa-new/hyperalignment/src/learn_hnet.py"
 
 
-# python3 $pyfile \
-#     --experiment-name="hnet_18-6_fmlp_c-32_bs-512_lr-1e-2" \
-#     --num-image-encoders=18 \
-#     --encoder-batch-size=6 \
-#     --hnet-cond-emb-dim=32 \
-#     --cond-type="features" \
-#     --num-epochs=20 \
-#     --learning-rate=1e-2 \
-#     --batch-size=512 \
-#     --random-seed=0 \
-#     --scheduler="off" \
-#     --warmup-steps=100 \
-#     --emb-loss=False \
-#     --normalize-output=True \
-#     --dataset-scale=1.0 \
-#     --hnet-decoder-type="mlp";
+python3 $pyfile \
+    --experiment-name="hnet_18-6_fmlp_c-32_bs-512_lr-1e-2" \
+    --num-image-encoders=18 \
+    --encoder-batch-size=6 \
+    --hnet-cond-emb-dim=32 \
+    --cond-type="features" \
+    --num-epochs=20 \
+    --learning-rate=1e-2 \
+    --batch-size=512 \
+    --random-seed=0 \
+    --scheduler="off" \
+    --warmup-steps=100 \
+    --emb-loss=False \
+    --normalize-output=True \
+    --dataset-scale=1.0 \
+    --hnet-decoder-type="mlp";
 
 # python3 $pyfile \
 #     --experiment-name="hnet_24-8_fmlp_c-32_bs-512_lr-1e-2" \
@@ -53,19 +53,19 @@ pyfile="/home/mila/s/sparsha.mishra/projects/hypa-new/hyperalignment/src/learn_h
 #     --chunk-dim=256 \
 #     --hnet-decoder-type="mlp";
 
-python3 $pyfile \
-    --experiment-name="hnet_30-10_fmlp_c-32_bs-512_lr-1e-2" \
-    --num-image-encoders=30 \
-    --encoder-batch-size=10 \
-    --hnet-cond-emb-dim=32 \
-    --cond-type="features" \
-    --num-epochs=20 \
-    --learning-rate=1e-2 \
-    --batch-size=512 \
-    --scheduler="off" \
-    --warmup-steps=100 \
-    --emb-loss=False \
-    --normalize-output=True \
-    --chunk-dim=256 \
-    --hnet-decoder-type="mlp";
+# python3 $pyfile \
+#     --experiment-name="hnet_30-10_fmlp_c-32_bs-512_lr-1e-2" \
+#     --num-image-encoders=30 \
+#     --encoder-batch-size=10 \
+#     --hnet-cond-emb-dim=32 \
+#     --cond-type="features" \
+#     --num-epochs=20 \
+#     --learning-rate=1e-2 \
+#     --batch-size=512 \
+#     --scheduler="off" \
+#     --warmup-steps=100 \
+#     --emb-loss=False \
+#     --normalize-output=True \
+#     --chunk-dim=256 \
+#     --hnet-decoder-type="mlp";
  
