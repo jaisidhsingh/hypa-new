ae5458ee0c253600a96be89617a6136f4ca7de3c
jaisidhsingh <jaisidhsingh@gmail.com>
2025-01-29 05:52:30 +0100

    ape roberta eval

--- hyperalignment/src/ape_evaluation.py
+++ hyperalignment/src/ape_evaluation.py
@@ -120,8 +120,8 @@ if __name__ == "__main__":
     args.exp_name = "deit3_large_patch16_384.fb_in22k_ft_in1k"
     # args.encoder_index = 0
     args.image_embed_dim = 1024
-    args.text_embed_dim = 384
-    args.text_encoder = "all-MiniLM-L12-v2"
+    args.text_embed_dim = 1024
+    args.text_encoder = "all-roberta-large-v1"
     args.num_encoders = 1
     args.encoder_batch = 1
     args.encoder_index = 3
